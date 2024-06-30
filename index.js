ca8ed7e3f5e210f3d9c7439cbd4f113e636c35fd
Ron Tep <ronaldtep+github@gmail.com>
2024-06-30 18:08:01 -0400

    Begin writing the JS for generating calculated results

--- index.js
+++ index.js
@@ -3,3 +3,14 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+const lengthResults = document.getElementById("length-results")
+const volumeResults = document.getElementById("volume-results")
+const massResults = document.getElementById("mass-results")
+
+const inputBox = document.getElementById("input-box")
+let userNumber = inputBox.value
+console.log(typeof userNumber)
+console.log(userNumber)
+// convert length
+ 
