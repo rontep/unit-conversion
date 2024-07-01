2cb50d275485240afdd6c0731cd4a0ad3b4c1be6
Ron Tep <ronaldtep+github@gmail.com>
2024-07-01 17:31:19 -0400

    Write functions to convert and render input in JS

--- index.js
+++ index.js
@@ -4,13 +4,56 @@
 1 kilogram = 2.204 pound
 */
 
-const lengthResults = document.getElementById("length-results")
-const volumeResults = document.getElementById("volume-results")
-const massResults = document.getElementById("mass-results")
-
+// store input box element 
 const inputBox = document.getElementById("input-box")
-let userNumber = inputBox.value
-console.log(typeof userNumber)
-console.log(userNumber)
-// convert length
- 
+const convertBtn = document.getElementById("convert-btn")
+
+// convert a particular category
+function convert(category, input) {
+    let toImperial = 0
+    let metUnit = ""
+    let impUnit = ""
+    switch (category) {
+        case "length":
+            toImperial = 3.281
+            metUnit = "meters"
+            impUnit = "feet"
+            break
+        case "volume":
+            toImperial = 0.264
+            metUnit = "liters"
+            impUnit = "gallons"
+            break
+        case "mass":
+            toImperial = 2.204
+            metUnit = "kilos"
+            impUnit = "pounds"
+            break
+    }  
+    let toImp = input * toImperial
+    toImp = toImp.toFixed(3)
+    let toMet = input / toImperial
+    toMet = toMet.toFixed(3)
+    if (input != Math.round(input)) {
+        input = input.toFixed(3)
+    }
+    const curId = `${category}-results`
+    const curEl = document.getElementById(curId)
+    console.log(curEl)
+    curEl.textContent = `${input} ${metUnit} = ${toImp} ${impUnit} | ${input} ${impUnit} = ${toMet} ${metUnit}`
+}
+
+// call conversion on each thing functions
+function renderConversion() {
+    let userNumber = inputBox.value
+    userNumber = Number(userNumber)
+    convert("length", userNumber)
+    convert("volume", userNumber)
+    convert("mass", userNumber)
+}
+
+// initial conversions
+renderConversion()
+
+// perform render when button pressed
+convertBtn.addEventListener("click", renderConversion)
